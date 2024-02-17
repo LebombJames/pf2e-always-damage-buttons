a47651cb2d510a50d490ba566687456c86abf14c
LebombJames <77904738+LebombJames@users.noreply.github.com>
2024-02-17 15:05:30 +0000

    Setup later to properly use i18n strings

--- always-damage-buttons.mjs
+++ always-damage-buttons.mjs
@@ -1,13 +1,14 @@
 let damageButtonHTML = "";
 
-Hooks.once("init", async () => {
+Hooks.once("i18nInit", async () => {
     const DamageRoll = CONFIG.Dice.rolls.find((R) => R.name === "DamageRoll");
     const damageChatCard = await renderTemplate(DamageRoll.CHAT_TEMPLATE, { showButtons: true });
 
     let dummy = document.createElement("a");
     dummy.innerHTML = damageChatCard;
 
-    damageButtonHTML = dummy.querySelector("section.damage-application").innerHTML;
+    damageButtonHTML = dummy.querySelector("section.damage-application").innerHTML;    
+    console.log("Pf2E Always Damage Buttons | Setup complete.");
 });
 
 Hooks.on("renderChatMessage", (doc, [html], options) => {
